4cdf426690a14dc8f926a30e04fd439a38ed4922
edibotopic <edibotopic@gmail.com>
2022-04-13 21:44:04 +0100

    temp change of dir name to retrack

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # dotfiles
+
 Personal config files for linux devices
+
+Having some issues with the neovim upload...
---
+++ nvim_temp/.config/nvim/init.lua
@@ -0,0 +1,29 @@
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.colorscheme"
+require "user.cmp"
+require "user.lsp"
+require "user.telescope"
+require "user.treesitter"
+require "user.autopairs"
+require "user.comment"
+require "user.gitsigns"
+require "user.nvim-tree"
+require "user.bufferline"
+require "user.lualine"
+require "user.toggleterm"
+require "user.project"
+require "user.impatient"
+require "user.indentline"
+require "user.alpha"
+require "user.whichkey"
+require "user.autocommands"
+
+--extra plugins
+require "user.bookmarks"
+--set default colorscheme
+vim.cmd[[colorscheme everforest]]
+--read quarto markdown as regular markdown
+vim.cmd[[autocmd BufRead,BufNew,BufNewFile *.qmd setlocal ft=markdown]]
+
