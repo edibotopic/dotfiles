c79531da30f3742f7ae772340f44df4cdeeeb48c
edibotopic <edibotopic@gmail.com>
2022-08-22 19:57:38 +0100

    added vale support

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -51,8 +51,8 @@ return packer.startup(function(use)
     --[[ use {"akinsho/bufferline.nvim", tag = 'v1.*'} ]]
     use {
         'romgrk/barbar.nvim',
-        requires = {'kyazdani42/nvim-web-devicons'}
-        }
+        requires = { 'kyazdani42/nvim-web-devicons' }
+    }
     use "moll/vim-bbye"
     use "nvim-lualine/lualine.nvim"
     use "akinsho/toggleterm.nvim"
@@ -121,16 +121,27 @@ return packer.startup(function(use)
     use 'davidgranstrom/nvim-markdown-preview'
     -- use 'iamcco/markdown-preview.nvim'
     use({
-       "quarto-dev/quarto-vim",
-       requires = {
-          {"vim-pandoc/vim-pandoc-syntax"},
-       },
-       ft = {"quarto"},
+        "quarto-dev/quarto-vim",
+        requires = {
+            { "vim-pandoc/vim-pandoc-syntax" },
+        },
+        ft = { "quarto" },
     })
     use { 'rrethy/vim-hexokinase', run = 'make hexokinase' }
     use 'lepture/vim-jinja'
     use 'https://github.com/PolyCement/vim-tweego'
     use 'jbyuki/venn.nvim'
+    use {
+        "folke/trouble.nvim",
+        requires = "kyazdani42/nvim-web-devicons",
+        config = function()
+            require("trouble").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
     -- use {
     --     'glacambre/firenvim',
     --     run = function() vim.fn['firenvim#install'](0) end
