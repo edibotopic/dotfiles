1aa14fb97ce5b18d12e5728b1f4495106651bdd8
edibotopic <edibotopic@gmail.com>
2024-03-18 12:03:30 +0000

    Cursor is always a block

--- nvim-kick/.config/nvim-kick/init.lua
+++ nvim-kick/.config/nvim-kick/init.lua
@@ -74,6 +74,7 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 8
 
+vim.cmd("set guicursor=n-v-c-i:block")
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
