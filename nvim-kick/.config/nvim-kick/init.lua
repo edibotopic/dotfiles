e674b3b690dbe56de95489c175987510131fb7a1
edibotopic <edibotopic@gmail.com>
2024-07-13 12:44:22 +0100

    Cleanup

--- nvim-kick/.config/nvim-kick/init.lua
+++ nvim-kick/.config/nvim-kick/init.lua
@@ -164,33 +164,9 @@ require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 
-	"typicode/bg.nvim", -- Adapt terminal background on colorscheme change
-
 	{
-		"danymat/neogen", -- Auto-generate (some) annotations
-		config = true,
-		-- Uncomment next line if you want to follow only stable versions
-		-- version = "*"
-	},
-
-	{
-		"nvim-orgmode/orgmode",
-		event = "VeryLazy",
-		ft = { "org" },
-		config = function()
-			-- Setup orgmode
-			require("orgmode").setup({
-				org_agenda_files = "~/orgfiles/**/*",
-				org_default_notes_file = "~/orgfiles/refile.org",
-			})
-
-			-- NOTE: If you are using nvim-treesitter with `ensure_installed = "all"` option
-			-- add `org` to ignore_install
-			-- require('nvim-treesitter.configs').setup({
-			--   ensure_installed = 'all',
-			--   ignore_install = { 'org' },
-			-- })
-		end,
+		"typicode/bg.nvim",
+		event = { "ColorSchemePre", "VeryLazy" },
 	},
 
 	-- NOTE: Plugins can also be added by using a table,
@@ -759,26 +735,6 @@ require("lazy").setup({
 					end,
 				},
 			})
-		end,
-	},
-
-	{ -- You can easily change to a different colorscheme.
-		-- Change the name of the colorscheme plugin below, and then
-		-- change the command in the config to whatever the name of that colorscheme is
-		--
-		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-		-- "folke/tokyonight.nvim",
-		"kdheepak/monochrome.nvim",
-		priority = 1000, -- make sure to load this before all the other start plugins
-		init = function()
-			-- Load the colorscheme here.
-			-- Like many other themes, this one has different styles, and you could load
-			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("monochrome")
-
-			-- You can configure highlights by doing something like
-			vim.cmd.hi("Comment gui=none")
-			vim.cmd.hi("clear SignColumn")
 		end,
 	},
 
