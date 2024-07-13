9e04ab4d6fe82736c4e69bd2912d1ab2d4303787
edibotopic <edibotopic@gmail.com>
2024-07-13 12:38:59 +0100

    Remove old nvim config

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-vim.cmd [[
-try
-  colorscheme base16-dark-seasmells
-catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
-  set background=dark
-endtry
-]]
--- nvim/.config/nvim/lua/user/oil.lua
+++
@@ -1,9 +0,0 @@
-local status_ok, oil = pcall(require, 'oil')
-if not status_ok then
-  return
-end
-
-oil.setup{
-    vim.keymap.set("n", "-", oil.open, { desc = "Open parent directory" })
-}
-
