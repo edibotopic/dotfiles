1769a6f807a5ada3aaa261ca2d3aeb3333ccc318
edibotopic <edibotopic@gmail.com>
2024-09-19 09:12:44 +0100

    add conjure to nvim

--- nvim-kick/.config/nvim-kick/init.lua
+++ nvim-kick/.config/nvim-kick/init.lua
@@ -130,6 +130,31 @@ require("lazy").setup({
 	"tpope/vim-sleuth",
 
 	{
+		"Olical/conjure",
+		ft = { "clojure", "fennel", "racket", "janet" }, -- etc
+		lazy = true,
+		init = function()
+			-- Set configuration options here
+			-- Uncomment this to get verbose logging to help diagnose internal Conjure issues
+			-- This is VERY helpful when reporting an issue with the project
+			-- vim.g["conjure#debug"] = true
+		end,
+
+		-- Optional cmp-conjure integration
+		dependencies = { "PaterJason/cmp-conjure" },
+	},
+	{
+		"PaterJason/cmp-conjure",
+		lazy = true,
+		config = function()
+			local cmp = require("cmp")
+			local config = cmp.get_config()
+			table.insert(config.sources, { name = "conjure" })
+			return cmp.setup(config)
+		end,
+	},
+
+	{
 		"typicode/bg.nvim",
 		event = { "ColorSchemePre", "VeryLazy" },
 	},
